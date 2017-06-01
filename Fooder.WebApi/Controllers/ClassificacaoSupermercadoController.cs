f09200939428ce4c21136487bed580b5e23ffc43
Túlio Rezende <tulio.mamute@hotmail.com>
2017-05-31 22:21:08 -0300

    Criação do calculo, por supermercado

--- Fooder.WebApi/Controllers/ClassificacaoSupermercadoController.cs
+++ Fooder.WebApi/Controllers/ClassificacaoSupermercadoController.cs
@@ -19,8 +19,21 @@ namespace Fooder.WebApi.Controllers
         /// </summary>
         /// <param name="ListaProdutos"></param>
         /// <returns></returns>
-        public IHttpActionResult Get(ProdutosLista ListaProdutos)
+        public IHttpActionResult Get(ProdutosLista[] ListaProdutos)
         {
+            int[] CodigoProdutos = ListaProdutos.Select(x => x.CodigoProduto).Distinct().ToArray();
+
+            List<ESTOQUE> elementos_estoque = db.ESTOQUE.Where(x => CodigoProdutos.Contains(x.PRODUTO_ID)).ToList();
+            int[] Mercados = db.ESTOQUE.Select(x => x.MERCADO_ID).Distinct().ToArray();
+
+            foreach (int itemMercado in Mercados)
+            {
+                elementos_estoque
+                    .Where(x => x.MERCADO_ID == itemMercado)
+                    .Join(ListaProdutos, a => a.PRODUTO_ID, b => b.CodigoProduto, (a, b) => new { SomaProduto = a.PRECO * b.QuantidadeProduto, Produto = a.PRODUTO_ID, Mercado = a.MERCADO_ID })
+                    .Sum(x => x.SomaProduto);
+            }
+
             return null;
         }
     }
